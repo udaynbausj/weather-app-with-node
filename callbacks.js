16028645f7aab82f8b8d3249c55402970b5e884d
Uday BK <uday.nbausj@gmail.com>
2018-11-05 10:53:40 +0530

    Add files via upload

--- callbacks.js
+++ callbacks.js
@@ -1,30 +1,8 @@
 const request = require('request');
 const yargs = require('yargs');
-
+const geocode = require('./geocode');
 
 var argv = yargs.argv;
 
-var Encoded_location = encodeURI(argv.title);
 
-console.log(Encoded_location);
-
-request({
-    url : 'http://www.mapquestapi.com/geocoding/v1/address?key=R03EHcm0zfj8YWyhEcQnhAbnSa67aoNw\n' +
-        `\n&location=${Encoded_location}`,
-    json : true
-},(error,response,body) =>{
-    //console.log(JSON.stringify(body,undefined,4));
-    //console.log(body.results[0].locations[0].latLng);
-    if(error){
-        console.log('Unable to reach the server :(');
-        //window.alert('Unable to reach the server :(');
-    }else if(body.status === 'ZERO_RESULTS'){
-        console.log('Please enter valid address');
-        //window.alert('Please enter valid address');
-    }else{
-        //window.alert(`Latitude is : ${body.results[0].locations[0].latLng.lat}`);
-        //window.alert(`Longitude is : ${body.results[0].locations[0].latLng.lng}`);
-        console.log(`Latitude is : ${body.results[0].locations[0].latLng.lat}`);
-        console.log(`Longitude is : ${body.results[0].locations[0].latLng.lng}`);
-    }
-});
+geocode.geocodeAddress(argv.title);
